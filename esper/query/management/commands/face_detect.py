7ec4b3c033f0831c755ac085c899cbb11fdfc228
Will Crichton <wcrichto@stanford.edu>
2017-05-11 21:35:51 -0400

    Added torch

--- esper/query/management/commands/face_detect.py
+++ esper/query/management/commands/face_detect.py
@@ -3,6 +3,7 @@ from query.models import Video, Face
 from scannerpy import Database, DeviceType, Job
 from scannerpy.stdlib import parsers, pipelines
 import os
+import cv2
 
 class Command(BaseCommand):
     help = 'Detect faces in videos'
@@ -25,14 +26,21 @@ class Command(BaseCommand):
 
             c = db.new_collection('tmp', filtered, force=True)
             faces_c = pipelines.detect_faces(
-                db, c, lambda t: t.range(0, 100), 'tmp_faces')
+                db, c, lambda t: t.all(), 'tmp_faces')
 
-            for path, video_faces in zip(filtered, faces_c.tables()):
+            for path, video_faces_table in zip(filtered, faces_c.tables()):
                 video = Video.objects.filter(path=path).get()
-                for i, frame_faces in video_faces.load(['bboxes'], parsers.bboxes):
+                frames = db.table(path).load(['frame'])
+                video_faces = video_faces_table.load(['bboxes'], parsers.bboxes)
+                for (i, frame_faces), (_, frame) in zip(video_faces, frames):
                     for bbox in frame_faces:
                         f = Face()
                         f.video = video
                         f.frame = i
                         f.bbox = bbox
                         f.save()
+
+                        thumbnail_path = 'assets/thumbnails/{}_{}.jpg'.format(video.id, f.id)
+                        thumbnail = frame[0][int(bbox.y1):int(bbox.y2),
+                                             int(bbox.x1):int(bbox.x2)]
+                        cv2.imwrite(thumbnail_path, cv2.cvtColor(thumbnail, cv2.COLOR_RGB2BGR))
