104d3758af176d9a78b1b6366e52e4e0418466b3
Will Crichton <wcrichto@stanford.edu>
2017-05-24 15:52:49 -0400

    Improved video interface, added face cluster edit mode

--- esper/assets/js/views/video.jsx
+++ esper/assets/js/views/video.jsx
@@ -10,7 +10,13 @@ import {Button, Collapse} from 'react-bootstrap';
 class VideoView extends React.Component {
   constructor(...args) {
     super(...args);
-    this.state = {showAllFaces: false, frame: 0};
+    this.state = {
+      showAllFaces: false,
+      frame: 0,
+      editMode: false,
+      selectStart: null,
+      selections: []
+    };
   }
 
   render() {
@@ -30,17 +36,47 @@ class VideoView extends React.Component {
       ['Resolution', `${video.width}x${video.height}`]];
 
     let makeFace = (face, j) => {
-      let onClick = () => { this.setState({'frame': face.frame}); }
-      return <img key={j} src={`/static/thumbnails/${face.video}_${face.id}.jpg`} onClick={onClick}/>;
+      // TODO: highlight selected face in different color
+      let onClick = () => {
+        if (this.state.editMode) {
+          if (this.state.selectStart == null) {
+            this.setState({selectStart: face});
+          } else {
+            this.state.selections.push([this.state.selectStart, face]);
+            console.log(this.state.selections);
+            this.setState({selectStart: null});
+          }
+        } else {
+          this.setState({'frame': face.frame});
+        }
+      };
+
+      let colors = [
+        'red',
+        'green',
+        'blue',
+        'yellow']
+
+      let selected = false;
+      let color = {};
+      for (var i = 0; i < this.state.selections.length; ++i) {
+        let [start, end] = this.state.selections[i];
+        if (face.frame >= start.frame && face.frame <= end.frame &&
+            face.identity == start.identity) {
+          selected = true;
+          color = {'borderColor': colors[i % colors.length]};
+          break;
+        }
+      }
+      return <img key={j} src={`/static/thumbnails/${face.video}_${face.id}.jpg`} onClick={onClick}
+                  className={selected ? 'selected' : ''} style={color} />;
     };
 
     return (
       <div className="video">
         <div className="row">
-          <div className="pull-left">
+          <div className="video-meta col-md-4">
             <VideoSummary store={video} frame={this.state.frame} show_meta={false} />
-          </div>
-          <div className="pull-right">
             <h2>Metadata</h2>
             <table className="table">
               <tbody>
@@ -49,41 +85,46 @@ class VideoView extends React.Component {
                  )}
               </tbody>
             </table>
+            <Button onClick={() => this.setState({editMode: !this.state.editMode})}>
+              {this.state.editMode ? 'View mode' : 'Edit mode'}
+            </Button>
+          </div>
+          <div className="video-faces col-md-8">
+            {video.loadedFaces
+             ? (<div>
+               <div className="cluster">
+                 <h3>All faces
+                   &nbsp;
+                   <Button onClick={() => this.setState({showAllFaces: !this.state.showAllFaces})}>
+                     +
+                   </Button>
+                 </h3>
+                 <Collapse in={this.state.showAllFaces}>
+                   <div>
+                     {_.map(video.faces, (faces, i) =>
+                       <div className="frame" key={i}>
+                         {faces.map(makeFace)}
+                       </div>)}
+                   </div>
+                 </Collapse>
+               </div>
+               <div className="clusters">
+                 {_.map(video.ids, (faces, i) =>
+                   <div className={'cluster ' + (this.state.editMode ? 'editing' : '')} key={i}>
+                     <h3>Cluster {i}</h3>
+                     {faces.map((entry, j) => {
+                        let [frame, face_index] = entry;
+                        let frame_faces = video.faces[frame];
+                        let face = frame_faces[face_index];
+                        return makeFace(face, j);
+                      })}
+                   </div>
+                  )}
+               </div>
+             </div>)
+             : (<div>Loading faces...</div>)}
           </div>
         </div>
-        {video.loadedFaces
-         ? (<div>
-           <div className="cluster">
-             <h3>All faces
-               &nbsp;
-               <Button onClick={() => this.setState({showAllFaces: !this.state.showAllFaces})}>
-                 +
-               </Button>
-             </h3>
-             <Collapse in={this.state.showAllFaces}>
-               <div>
-                 {_.map(video.faces, (faces, i) =>
-                   <div className="frame" key={i}>
-                     {faces.map(makeFace)}
-                   </div>)}
-               </div>
-             </Collapse>
-           </div>
-           <div className="clusters">
-             {_.map(video.ids, (faces, i) =>
-               <div className="cluster" key={i}>
-                 <h3>Cluster {i}</h3>
-                 {faces.map((entry, j) => {
-                    let [frame, face_index] = entry;
-                    let frame_faces = video.faces[frame];
-                    let face = frame_faces[face_index];
-                    return makeFace(face, j);
-                  })}
-               </div>
-              )}
-           </div>
-         </div>)
-         : (<div>Loading faces...</div>)}
       </div>
     );
   }
