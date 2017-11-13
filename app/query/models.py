455b74b0cfc1cc6b3d297bacea627f50ee3bea16
Will Crichton <wcrichto@cs.stanford.edu>
2017-11-13 21:33:53 +0000

    Fixed ipython not working

--- app/query/models.py
+++ app/query/models.py
@@ -4,7 +4,7 @@ import os
 import sys
 import importlib
 
-for dataset_path in glob("query/datasets/*/"):
+for dataset_path in glob("/app/query/datasets/*/"):
     dataset_name = dataset_path.split('/')[-2]
     with base.Dataset(dataset_name):
         importlib.import_module('query.datasets.{}.models'.format(dataset_name))
