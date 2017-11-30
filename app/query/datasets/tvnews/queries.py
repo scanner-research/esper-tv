1cb982333ed1cde03e097d1ebe16074a7ce0ca36
Will Crichton <wcrichto@cs.stanford.edu>
2017-11-30 05:57:20 +0000

    Substantially improved bbox diff checkers. Closes #65

--- app/query/datasets/tvnews/queries.py
+++ app/query/datasets/tvnews/queries.py
@@ -78,18 +78,18 @@ def faces_unlike_poppy_harlow():
     FaceFeatures.getTempFeatureModel([id])
     result = qs_to_result(
         Face.objects.filter(**{'facefeaturestemp__distto_{}__gte'.format(id): 1.7}).order_by(
-            'facefeaturestemp__distto_{}'.format(id)))
+                                   'facefeaturestemp__distto_{}'.format(id)))
 
 
-@query("Differing bounding boxes")
-def differing_bounding_boxes():
+@query("MTCNN missed face bboxes vs. handlabeled")
+def mtcnn_vs_handlabeled():
     labeler_names = [l['labeler__name'] for l in Face.objects.values('labeler__name').distinct()]
 
     videos = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
     for frame in Frame.objects.filter(
-            Q(video__show='Situation Room With Wolf Blitzer') | Q(
-                video__show='Special Report With Bret Baier')).filter(
-                    face__labeler__name='handlabeled').select_related('video')[:50000:5]:
+            Q(video__show='Situation Room With Wolf Blitzer') |
+            Q(video__show='Special Report With Bret Baier')).filter(
+                face__labeler__name='handlabeled').select_related('video').order_by('id')[:50000:5]:
         faces = list(Face.objects.filter(frame=frame).select_related('labeler'))
         has_mtcnn = any([f.labeler.name == 'mtcnn' for f in faces])
         has_handlabeled = any([f.labeler.name == 'handlabeled' for f in faces])
@@ -104,26 +104,27 @@ def differing_bounding_boxes():
     mistakes = defaultdict(lambda: defaultdict(tuple))
     for video, frames in videos.iteritems():
         for frame, labelers in frames.iteritems():
-            for labeler, faces in labelers.iteritems():
-                for face in faces:
-                    if bbox_area(face) < AREA_THRESHOLD:
-                        continue
+            labeler = 'handlabeled'
+            faces = labelers[labeler]
+            for face in faces:
+                if bbox_area(face) < AREA_THRESHOLD:
+                    continue
 
-                    mistake = True
-                    for other_labeler in labeler_names:
-                        if labeler == other_labeler: continue
-                        other_faces = labelers[other_labeler] if other_labeler in labelers else []
-                        for other_face in other_faces:
-                            if bbox_dist(face, other_face) < DIST_THRESHOLD:
-                                mistake = False
-                                break
-
-                        if mistake and len(other_faces) > 0:
-                            mistakes[video][frame] = (faces, other_faces)
+                mistake = True
+                for other_labeler in labeler_names:
+                    if labeler == other_labeler: continue
+                    other_faces = labelers[other_labeler] if other_labeler in labelers else []
+                    for other_face in other_faces:
+                        if bbox_dist(face, other_face) < DIST_THRESHOLD:
+                            mistake = False
                             break
-                    else:
-                        continue
-                    break
+
+                    if mistake:
+                        mistakes[video][frame] = (faces, other_faces)
+                        break
+                else:
+                    continue
+                break
 
     result = []
     for video, frames in list(mistakes.iteritems())[:100]:
@@ -134,33 +135,100 @@ def differing_bounding_boxes():
                 'objects': [bbox_to_dict(f) for f in faces + other_faces]
             })
 
-    return {'result': result, 'count': len(result), 'type': 'Frame'}
+    return simple_result(result, 'Frame')
+
+
+@query("MTCNN missed face bboxes vs. OpenPose")
+def mtcnn_vs_openpose():
+    labeler_names = ['mtcnn', 'openpose']
+
+    videos = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    frames = Frame.objects.all() \
+        .annotate(c=Subquery(
+            Pose.objects.filter(frame=OuterRef('pk'), bbox_score__gt=0).values('frame') \
+            .annotate(c=Count('*')).values('c'), models.IntegerField())) \
+        .filter(c__gt=0) \
+        .select_related('video') \
+        .order_by('id')
+    for frame in frames[:50000:5]:
+        faces = list(Face.objects.filter(frame=frame))
+        poses = list(Pose.objects.filter(frame=frame, bbox_score__gt=0))
+        for face in faces:
+            videos[frame.video.id][frame.id]['mtcnn'].append(face)
+        for pose in poses:
+            videos[frame.video.id][frame.id]['openpose'].append(pose)
+
+    AREA_THRESHOLD = 0.02
+    DIST_THRESHOLD = 0.10
+
+    mistakes = defaultdict(lambda: defaultdict(tuple))
+    for video, frames in videos.iteritems():
+        for frame, labelers in frames.iteritems():
+            labeler = 'openpose'
+            faces = labelers[labeler]
+            for face in faces:
+                if bbox_area(face) < AREA_THRESHOLD:
+                    continue
+
+                mistake = True
+                for other_labeler in labeler_names:
+                    if labeler == other_labeler: continue
+                    other_faces = labelers[other_labeler] if other_labeler in labelers else []
+                    for other_face in other_faces:
+                        if bbox_dist(face, other_face) < DIST_THRESHOLD:
+                            mistake = False
+                            break
+
+                    if mistake and len(other_faces) > 0:
+                        mistakes[video][frame] = (faces, other_faces)
+                        break
+                else:
+                    continue
+                break
+
+    result = []
+    for video, frames in list(mistakes.iteritems())[:100]:
+        for frame, (faces, other_faces) in frames.iteritems():
+            result.append({
+                'video': video,
+                'start_frame': frame,
+                'objects': [bbox_to_dict(f) for f in other_faces + faces]
+            })
+
+    return simple_result(result, 'Frame')
+
 
 @query("People sitting")
 def people_sitting():
     def is_sitting(kp):
         def ang(v):
             return math.atan2(v[1], v[0]) / math.pi * 180
+
         def is_angled(v):
             v /= np.linalg.norm(v)
             v[1] = -v[1]  # correct for image coordinates
             a = ang(v)
             return a > 0 or a < -140
-        return is_angled(kp[Pose.LKnee] - kp[Pose.LHip]) or is_angled(kp[Pose.RKnee] - kp[Pose.RHip])
-        
+
+        return is_angled(kp[Pose.LKnee] - kp[Pose.LHip]) or is_angled(kp[Pose.RKnee] -
+                                                                      kp[Pose.RHip])
+
     frames_qs = Frame.objects.filter(video__channel='CNN') \
         .annotate(
             pose_count=Subquery(
-                Pose.objects.filter(frame=OuterRef('pk')).values('frame').annotate(c=Count('*')).values('c')), 
+                Pose.objects.filter(frame=OuterRef('pk')).values('frame').annotate(c=Count('*')).values('c')),
             woman_count=Subquery(
-                Face.objects.filter(frame=OuterRef('pk'), gender__name='female').values('frame').annotate(c=Count('*')).values('c'), 
+                Face.objects.filter(frame=OuterRef('pk'), gender__name='female').values('frame').annotate(c=Count('*')).values('c'),
                 output_field=models.IntegerField())) \
         .filter(pose_count__gt=0, pose_count__lt=6, woman_count__gt=0).order_by('id').select_related('video')
-        
-    frames = []        
+
+    frames = []
     for frame in frames_qs[:100000:10]:
-        filtered = filter_poses('pose', is_sitting, [Pose.LAnkle, Pose.LKnee, Pose.RAnkle, Pose.RKnee, Pose.RHip, Pose.LHip], poses=Pose.objects.filter(frame=frame))
-        
+        filtered = filter_poses(
+            'pose',
+            is_sitting, [Pose.LAnkle, Pose.LKnee, Pose.RAnkle, Pose.RKnee, Pose.RHip, Pose.LHip],
+            poses=Pose.objects.filter(frame=frame))
+
         if len(filtered) > 0:
             frames.append((frame, filtered))
 
@@ -170,17 +238,23 @@ def people_sitting():
         'objects': [pose_to_dict(p) for p in poses]
     } for (frame, poses) in frames], 'Frame')
 
+
 @query("Obama pictures")
 def obama_pictures():
     id = 3938394
     FaceFeatures.compute_distances(id)
-    sq = Face.objects.filter(track=OuterRef('pk'), labeler__name='mtcnn', facefeatures__distto__lte=1.0).values('track').annotate(c=Count('*'))
+    sq = Face.objects.filter(
+        track=OuterRef('pk'), labeler__name='mtcnn',
+        facefeatures__distto__lte=1.0).values('track').annotate(c=Count('*'))
     out_tracks = []
 
     def close(x, y):
         return abs(x - y) < 0.02
 
-    tracks = list(FaceTrack.objects.annotate(duration=Track.duration(), c=Subquery(sq.values('c'), models.IntegerField())).filter(duration__gt=0, c__gt=0))
+    tracks = list(
+        FaceTrack.objects.annotate(
+            duration=Track.duration(), c=Subquery(sq.values('c'), models.IntegerField())).filter(
+                duration__gt=0, c__gt=0))
     faces = list(Face.objects.filter(track__in=tracks).select_related('frame'))
     face_tracks = {t.id: (t, []) for t in tracks}
     for f in faces:
@@ -189,11 +263,11 @@ def obama_pictures():
     for track, faces in face_tracks.values():
         faces.sort(lambda a, b: a.frame.number - b.frame.number)
         valid = True
-        for i in range(len(faces)-1):
-            if not (close(faces[i].bbox_x1, faces[i+1].bbox_x1) and
-                close(faces[i].bbox_y1, faces[i+1].bbox_y1) and
-                close(faces[i].bbox_x2, faces[i+1].bbox_x2) and
-                close(faces[i].bbox_y2, faces[i+1].bbox_y2)):
+        for i in range(len(faces) - 1):
+            if not (close(faces[i].bbox_x1, faces[i + 1].bbox_x1)
+                    and close(faces[i].bbox_y1, faces[i + 1].bbox_y1)
+                    and close(faces[i].bbox_x2, faces[i + 1].bbox_x2)
+                    and close(faces[i].bbox_y2, faces[i + 1].bbox_y2)):
                 valid = False
                 break
         if valid:
