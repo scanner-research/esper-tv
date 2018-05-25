96d7a4d752a475c6437987e2219b12aebbbdc040
Will Crichton <wcrichto@cs.stanford.edu>
2018-05-25 23:12:05 +0000

    Uncomment sql dump

--- app/esper/sql_dump.py
+++ app/esper/sql_dump.py
@@ -4,15 +4,15 @@ import django.apps
 
 models = [m._meta.db_table for m in django.apps.apps.get_models(include_auto_created=True)]
 
-# with Timer('Exporting models'):
-#     def export_model(model):
-#         try:
-#             sp.check_call("/app/scripts/export-table.sh {}".format(model), shell=True)
-#         except Exception:
-#             import traceback
-#             print(model)
-#             traceback.print_exc()
-#     par_for(export_model, models, workers=8)
+with Timer('Exporting models'):
+    def export_model(model):
+        try:
+            sp.check_call("/app/scripts/export-table.sh {}".format(model), shell=True)
+        except Exception:
+            import traceback
+            print(model)
+            traceback.print_exc()
+    par_for(export_model, models, workers=8)
 
 with Timer('Ingest into Spark'):
     def transfer_model_spark(model):
@@ -20,13 +20,13 @@ with Timer('Ingest into Spark'):
         spark.save(model, df)
     par_for(transfer_model_spark, models, workers=8)
 
-# with Timer('Ingest into BigQuery'):
-#     sp.check_call('bq rm -r -f tvnews && bq mk tvnews', shell=True)
-#     def transfer_model_bq(model):
-#         try:
-#             sp.check_call("/app/scripts/transfer-to-bigquery.sh {}".format(model), shell=True)
-#         except Exception:
-#             import traceback
-#             print(model)
-#             traceback.print_exc()
-#     par_for(transfer_model_bq, models, workers=8)
+with Timer('Ingest into BigQuery'):
+    sp.check_call('bq rm -r -f tvnews && bq mk tvnews', shell=True)
+    def transfer_model_bq(model):
+        try:
+            sp.check_call("/app/scripts/transfer-to-bigquery.sh {}".format(model), shell=True)
+        except Exception:
+            import traceback
+            print(model)
+            traceback.print_exc()
+    par_for(transfer_model_bq, models, workers=8)
