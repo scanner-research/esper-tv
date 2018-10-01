59f3366c35307a8e5a52240cb990f021386be870
Will Crichton <wcrichto@cs.stanford.edu>
2018-10-01 05:31:50 +0000

    Remove esper_jupyter from main esper repo

--- app/esper_jupyter/esper_jupyter/__init__.py
+++
@@ -1,9 +0,0 @@
-from .main import *
-
-def _jupyter_nbextension_paths():
-    return [{
-        'section': 'notebook',
-        'src': 'static',
-        'dest': 'esper_jupyter',
-        'require': 'esper_jupyter/extension'
-    }]
