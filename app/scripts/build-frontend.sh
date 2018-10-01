44cf5ac1abe532cee4067658554f4301896f8140
Will Crichton <wcrichto@cs.stanford.edu>
2018-10-01 17:58:07 +0000

    Update build frontend script

--- app/scripts/build-frontend.sh
+++ app/scripts/build-frontend.sh
@@ -4,15 +4,19 @@ set -e
 pushd .
 
 cd /opt/vgrid
+git pull
 npm install
 npm run prepublish
 
 cd /opt/vgrid_jupyter/js
+git pull
 npm install
+npm link vgrid
 npm run prepublish
 
 cd /app
 npm install
+npm link vgrid
 npm run prepublish
 
 popd
