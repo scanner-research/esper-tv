d5c9479b06169cb10b85bb8eb88528fea88880b1
Dan Fu <danfu09@gmail.com>
2018-11-07 19:15:39 +0000

    Add parallel scan functionality

--- app/esper/captions.py
+++ app/esper/captions.py
@@ -129,5 +129,59 @@ def get_lowercase_segments(video_ids=None):
     with Pool(os.cpu_count()) as pool:
         results = pool.map(_get_lowercase_segments, video_ids)
     return zip(video_ids, results)
-        
-                           
+
+NGRAM_LEXICON_IDS = None
+
+def _scan_for_ngrams_in_parallel(video_id, verbose=None):
+    ngram_intervals = [[] for _ in NGRAM_LEXICON_IDS]
+    doc_id = VIDEO_ID_TO_DOCUMENT_ID.get(video_id, None)
+    if doc_id is None:
+        if verbose:
+            print('No document for video id: {}'.format(video_id), file=sys.stderr)
+        return ngram_intervals
+    for interval in DOCUMENT_DATA.token_intervals(doc_id, 0, DOCUMENTS[doc_id].duration):
+        found_token = [[False for _ in ids] for ids in NGRAM_LEXICON_IDS]
+        for token in interval.tokens:
+            for i, ngram in enumerate(NGRAM_LEXICON_IDS):
+                for j, ngram_id in enumerate(ngram):
+                    if token == ngram_id:
+                        found_token[i][j] = True
+        for i, token_markers in enumerate(found_token):
+            found = True
+            for marker in token_markers:
+                if not marker:
+                    found = False
+                    break
+            if found:
+                ngram_intervals[i].append((interval.start, interval.end))
+
+    return ngram_intervals
+
+def scan_for_ngrams_in_parallel(ngram_list, video_ids=None):
+    """
+    Scans through video transcripts for the terms in the ngrams.
+
+    ngramlist is a list of ngrams to search for, for example
+    ["JOINING US NOW", "VERMONT SENATOR", "THIS IS CNN"].
+
+    This function scans through the transcripts of the videos in video_ids and
+    returns a list of tuples where the first tuple is the video id, and the
+    second tuple is a list of lists of intervals, one list for every ngram.
+
+    Will return all intervals where all words in the ngram appear.
+    """
+    if video_ids is None:
+        video_ids = [v.id for v in Video.objects.filter(threeyears_dataset=True)]
+    elif not isinstance(video_ids, list):
+        video_ids = list(video_ids)
+
+    ngram_lexicon_ids = [
+        [LEXICON[ngram].id for ngram in ngrams.split(" ")]
+        for ngrams in ngram_list
+    ]
+    global NGRAM_LEXICON_IDS
+    NGRAM_LEXICON_IDS = ngram_lexicon_ids
+
+    with Pool(os.cpu_count()) as pool:
+        results = pool.map(_scan_for_ngrams_in_parallel, video_ids)
+    return zip(video_ids, results)
