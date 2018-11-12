db1f6e35bd8617a8a0482c1cc91da83d2bfac435
Dan Fu <danfu09@gmail.com>
2018-11-12 18:55:45 +0000

    Update example rekall queries

--- app/esper/queries.py
+++ app/esper/queries.py
@@ -1084,43 +1084,43 @@ def shots_with_host_and_still_face():
 @query('Hand-labeled Interviews (Sandbox)')
 def handlabeled_interviews():
     from query.models import LabeledInterview
-    from esper.temporal_rangelist_util import qs_to_trlists, trlists_to_result
+    from esper.rekall import qs_to_intrvllists, intrvllists_to_result
 
     interviews = LabeledInterview.objects \
             .annotate(fps=F('video__fps')) \
             .annotate(min_frame=F('fps') * F('start')) \
             .annotate(max_frame=F('fps') * F('end'))
 
-    return trlists_to_result(qs_to_trlists(interviews))
+    return intrvllists_to_result(qs_to_intrvllists(interviews))
 
 @query('Hand-labeled Panels (Sandbox)')
 def handlabeled_panels():
     from query.models import LabeledPanel
-    from esper.temporal_rangelist_util import qs_to_trlists, trlists_to_result
+    from esper.rekall import qs_to_intrvllists, intrvllists_to_result
 
     panels = LabeledPanel.objects \
             .annotate(fps=F('video__fps')) \
             .annotate(min_frame=F('fps') * F('start')) \
             .annotate(max_frame=F('fps') * F('end'))
 
-    return trlists_to_result(qs_to_trlists(panels))
+    return intrvllists_to_result(qs_to_intrvllists(panels))
 
 @query('Hand-labeled Commercials (Sandbox)')
 def handlabeled_commercials():
     from query.models import LabeledCommercial
-    from esper.temporal_rangelist_util import qs_to_trlists, trlists_to_result
+    from esper.rekall import qs_to_intrvllists, intrvllists_to_result
 
     commercials = LabeledCommercial.objects \
             .annotate(fps=F('video__fps')) \
             .annotate(min_frame=F('fps') * F('start')) \
             .annotate(max_frame=F('fps') * F('end'))
 
-    return trlists_to_result(qs_to_trlists(commercials))
+    return intrvllists_to_result(qs_to_intrvllists(commercials))
 
 @query('Multiple Timelines (Sandbox)')
 def multiple_timelines():
     from query.models import LabeledInterview, LabeledPanel, LabeledCommercial
-    from esper.temporal_rangelist_util import qs_to_trlists, trlists_to_result, add_trlists_to_result
+    from esper.rekall import qs_to_intrvllists, intrvllists_to_result, add_intrvllists_to_result
 
     interviews = LabeledInterview.objects \
             .annotate(fps=F('video__fps')) \
@@ -1135,8 +1135,8 @@ def multiple_timelines():
             .annotate(min_frame=F('fps') * F('start')) \
             .annotate(max_frame=F('fps') * F('end'))
 
-    result = trlists_to_result(qs_to_trlists(interviews))
-    add_trlists_to_result(result, qs_to_trlists(panels), color="blue")
-    add_trlists_to_result(result, qs_to_trlists(commercials), color="purple")
+    result = intrvllists_to_result(qs_to_intrvllists(interviews))
+    add_intrvllists_to_result(result, qs_to_intrvllists(panels), color="blue")
+    add_intrvllists_to_result(result, qs_to_intrvllists(commercials), color="purple")
 
     return result
