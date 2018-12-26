d69e8d1099e5268f44fd53872b50c90ca15299c5
Will Crichton <wcrichto@cs.stanford.edu>
2018-12-26 21:38:02 +0000

    Add GCE GPU setup script

---
+++ scripts/gce-gpu-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install NVIDIA GPU driver
+# https://cloud.google.com/compute/docs/gpus/add-gpus#install-driver-script
+
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-9-0; then
+  # The 16.04 installer works with 16.10.
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+  apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  apt-get update
+  apt-get install cuda-9-0 -y
+fi
+# Enable persistence mode
+nvidia-smi -pm 1
+
+
+# Install nvidia-docker
+# https://github.com/NVIDIA/nvidia-docker#ubuntu-140416041804-debian-jessiestretch
+
+# Add the package repositories
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+
+# Install nvidia-docker2 and reload the Docker daemon configuration
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
