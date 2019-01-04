57b2a5fbffad35c06d7454d011b2f42ba6851748
Haotian <haotianz@stanford.edu>
2019-01-04 05:09:09 +0000

    add single person sing a song

--- app/esper/supercut.py
+++ app/esper/supercut.py
@@ -1,4 +1,4 @@
-from django.db.models import F
+from django.db.models import F, Q
 # import rekall
 from esper.rekall import *
 from rekall.video_interval_collection import VideoIntervalCollection
@@ -26,7 +26,8 @@ import tempfile
 from tqdm import tqdm
 import multiprocessing
 from pydub import AudioSegment
-
+import pysrt
+import re
 
 
 # ============== Help functions ==============    
@@ -42,7 +43,7 @@ def count_intervals(intrvlcol):
     return num_intrvl
 
 
-def intrvlcol2list(intrvlcol, with_duration=False):
+def intrvlcol2list(intrvlcol, with_duration=True):
     interval_list = []
     for video_id, intrvllist in intrvlcol.intervals.items():
         if with_duration:
@@ -55,15 +56,20 @@ def intrvlcol2list(intrvlcol, with_duration=False):
     print("Get {} intervals from interval collection".format(len(interval_list)))
     return interval_list
 
-def random_sample_candidates(intervals, num_sample):
+
+def select_candidates(intervals, num_sample=1, filter='random'):
     durations = [i[-1] for i in intervals]
     median = np.median(durations)
     intervals_regular = [i for i in intervals if i[-1] > 0.5 * median and i[-1] < 1.5 * median]
     # Todo: if regular intervals are not enough
-    return random.sample(intervals_regular, num_sample)
+    if filter == 'random':
+        return random.sample(intervals_regular, num_sample)
+    elif filter == 'longest':
+        durations_regular = [i[-1] for i in intervals_regular]
+        return intervals_regular[np.argmax(durations_regular)]
 
-
-def stitch_video_temporal(intervals, out_path):
+    
+def stitch_video_temporal(intervals, out_path, out_duration=None, speed=0.8, dilation=0):
     def download_video_clip(i):
         video_id, sfid, efid = intervals[i][:3]
         video = Video.objects.filter(id=video_id)[0]
@@ -72,7 +78,20 @@ def stitch_video_temporal(intervals, out_path):
     
     # make supercut video 
     clip_paths = par_for(download_video_clip, [i for i in range(len(intervals))])
-    concat_videos(clip_paths, out_path)
+    tmp_path = tempfile.NamedTemporaryFile(suffix='.mp4').name
+    concat_videos(clip_paths, tmp_path)
+    if not out_duration is None or speed != 1.0:
+        if not out_duration is None:
+            duration = sum([i[-1] for i in intervals])
+            speed = duration / out_duration
+            print(duration, speed)
+        speed = max(0.5, speed)
+        speed = min(2.0, speed)
+        cmd = 'ffmpeg -y -i {} -filter_complex "[0:v]setpts={}*PTS[v];[0:a]atempo={}[a]" -map "[v]" -map "[a]" {}' \
+              .format(tmp_path, 1 / speed, speed, out_path)
+        os.system(cmd)
+    else:
+        os.rename(tmp_path, out_path)
 
     
 def make_montage_t(args):
@@ -165,33 +184,79 @@ def merge_video_audio(video_path, audio_path, out_path):
     cmd_avi2mp4 = 'ffmpeg -y -i {} -c:a aac -b:a 128k -c:v libx264 -crf 23 {}'.format(tmp_path, out_path)
     os.system(cmd_merge)
     os.system(cmd_avi2mp4)
+
+    
+def concat_videos_simple(paths, out_path):
+    tmp_path = tempfile.NamedTemporaryFile(suffix='.txt').name
+    file = open(tmp_path, 'w')
+    for p in paths:
+        file.write('file ' + "\'" + p + "\'" + '\n')
+    file.close()
+    cmd = 'ffmpeg -y -safe 0 -f concat -i {} -c copy {}'.format(tmp_path, out_path)
+    os.system(cmd)
     
     
 # ============== Queries with rekall ==============    
-def get_person_intrvlcol(person_name, video_ids=None):
-    if video_ids is None:
-        videos = Video.objects.filter(threeyears_dataset=True)
-        video_ids = [video.id for video in videos]
+def get_person_intrvlcol(person_name, **kwargs):
+#     if video_ids is None:
+#         videos = Video.objects.filter(threeyears_dataset=True)
+#         video_ids = [video.id for video in videos]
     
-    all_faces = FaceIdentity.objects.annotate(height=F("face__bbox_y2") - F("face__bbox_y1"))
-    if not video_ids is None:
-        all_faces = all_faces.filter(face__shot__video_id__in=video_ids)
+#     faceIDs = FaceIdentity.objects \
+#               .annotate(video_id=F("face__frame__video_id")) \
+#               .annotate(shot_boundary=F("face__frame__shot_boundary"))
+            
+#     if not video_ids is None:
+#         faceIDs = faceIDs.filter(video_id__in=video_ids, shot_boundary=True) \
+    
+    if kwargs['labeler'] == 'old': # old labeler model
+        faceIDs = FaceIdentity.objects \
+                  .exclude(face__shot__isnull=True) \
+                  .filter(Q(labeler__name='face-identity-converted:'+person_name.lower()) | 
+                          Q(labeler__name='face-identity:'+person_name.lower()) ) \
+                  .filter(probability__gt=0.9) \
+                  .annotate(height=F("face__bbox_y2") - F("face__bbox_y1"))   
+        if 'large_face' in kwargs:
+            faceIDs = faceIDs.filter(height__gte=0.3)
+    
+        person_intrvllists = qs_to_intrvllists(
+            faceIDs.annotate(video_id=F("face__shot__video_id"))
+                   .annotate(shot_id=F("face__shot_id"))
+                   .annotate(min_frame=F("face__shot__min_frame"))
+                   .annotate(max_frame=F("face__shot__max_frame")),\
+            schema={
+                'start': 'min_frame',
+                'end': 'max_frame',
+                'payload': 'shot_id'
+            })
+        person_intrvlcol = VideoIntervalCollection(person_intrvllists)
+    else: # new labeler model
+        faceIDs = FaceIdentity.objects \
+                  .filter(face__frame__shot_boundary=False) \
+                  .filter(Q(labeler__name='face-identity-converted:'+person_name.lower()) | 
+                          Q(labeler__name='face-identity:'+person_name.lower()) ) \
+                  .filter(probability__gt=0.9) \
+                  .annotate(height=F("face__bbox_y2") - F("face__bbox_y1"))   
+        if 'large_face' in kwargs:
+            faceIDs = faceIDs.filter(height__gte=0.3)
+    
+        person_intrvllists_raw = qs_to_intrvllists(
+            faceIDs.annotate(video_id=F("face__frame__video_id"))
+                   .annotate(frame_id=F("face__frame__number"))
+                   .annotate(min_frame=F("face__frame__number"))
+                   .annotate(max_frame=F("face__frame__number") + 1),\
+            schema={
+                'start': 'min_frame',
+                'end': 'max_frame',
+                'payload': 'frame_id'
+            })
+        # dilate and coalesce
+        person_intrvllists = {}
+        for video_id, intrvllist in person_intrvllists_raw.items():
+            video = Video.objects.filter(id=video_id)[0]
+            person_intrvllists[video_id] = intrvllist.dilate(int(video.fps*1.6)).coalesce()
+        person_intrvlcol = VideoIntervalCollection(person_intrvllists)
         
-    person_intrvllists = qs_to_intrvllists(
-        all_faces
-            .filter(identity__name=person_name.lower(), 
-                    probability__gt=0.99,
-                    height__gte=0.3)
-            .annotate(video_id=F("face__shot__video_id"))
-            .annotate(shot_id=F("face__shot_id"))
-            .annotate(min_frame=F("face__shot__min_frame"))
-            .annotate(max_frame=F("face__shot__max_frame")),
-        schema={
-            'start': 'min_frame',
-            'end': 'max_frame',
-            'payload': 'shot_id'
-        })
-    person_intrvlcol = VideoIntervalCollection(person_intrvllists)
     print("Get {} intervals for person {}".format(count_intervals(person_intrvlcol), person_name))
     return person_intrvlcol
 
@@ -257,26 +322,69 @@ def get_oneface_intrvlcol(relevant_shots):
 
 def get_person_alone_phrase_intrvlcol(person_intrvlcol, phrase):
     phrase_intrvlcol = get_caption_intrvlcol(phrase, person_intrvlcol.get_allintervals().keys())
-    person_phrase_intrvlcol = person_intrvlcol.overlaps(phrase_intrvlcol)
+    person_phrase_intrvlcol_raw = person_intrvlcol.overlaps(phrase_intrvlcol)
+    # only keep intervals which is the same before overlap
+    person_phrase_intrvlcol = person_phrase_intrvlcol_raw.filter_against(
+        phrase_intrvlcol,
+        predicate = equal() )
+    
     relevant_shots = get_relevant_shots(person_phrase_intrvlcol)
     oneface_intrvlcol = get_oneface_intrvlcol(relevant_shots)
     person_alone_phrase_intrvlcol = person_phrase_intrvlcol.overlaps(oneface_intrvlcol)
+    
+    # Todo: run optical flow to filter out still images
+    
     
     print('Get {} person alone intervals for phrase \"{}\"'.format(count_intervals(person_alone_phrase_intrvlcol), phrase))
     return person_alone_phrase_intrvlcol
     
     
-# ============== Applications ==============    
-def same_person_one_sentence(person, sentence):
-    if type(person) == str:
-        person_intrvlcol = get_person_intrvlcol(person_name)
-    else:
-        person_intrvlcol = person
-    words = [word.upper() for word in sentence.split(' ')]
+# ============== Applications ============== 
+def single_person_one_song(person_name, lyric_path, out_path, person_intrvlcol=None, cache=True):
+    def time2second(time):
+        return time[0]*3600 + time[1]*60 + time[2] + time[3] / 1000.0
 
+    if person_intrvlcol is None:
+        person_intrvlcol = get_person_intrvlcol(person_name, large_face=True, labeler='old')
+    person_name = person_name.replace(' ', '_')
+    
+    # load lyrics
+    song_name = os.path.basename(lyric_path).replace('.srt', '')
+    lyrics = []
+    subs = pysrt.open(lyric_path)
+    for sub in subs:
+        lyrics.append((sub.text, time2second(tuple(sub.start)[:4]), time2second(tuple(sub.end)[:4])))
+
+    cache_path = '/app/result/supercut/cache/{}.pkl'.format(person_name)
+    phrase2interval = pickle.load(open(cache_path, 'rb')) if cache and os.path.exists(cache_path) else {}
+    
+    cutting_paths = []
+    for idx, (sentence, start, end) in enumerate(lyrics):
+        supercut_candidates = single_person_one_sentence(person_intrvlcol, sentence, phrase2interval)
+        pickle.dump(phrase2interval, open(cache_path, 'wb'))
+        # sample candidate
+        supercut_intervals = [select_candidates(candidates, filter='longest') for candidates in supercut_candidates]
+        # concat clips with dilation at the end; global speed change
+        tmp_path = '/app/result/supercut/{}-{}-{}.mp4'.format(person_name, song_name, idx) 
+        stitch_video_temporal(supercut_intervals, tmp_path, out_duration=None)
+        cutting_paths.append(tmp_path)
+#         if idx == 1:
+#             break
+    concat_videos(cutting_paths, out_path)
+    
+
+def single_person_one_sentence(person_intrvlcol, sentence, phrase2interval=None, concat_word=4):
+    words = [word.upper() for word in sentence.replace(',', '').replace('.', '').replace('!', '').split(' ')]
+    
+    # Magic numbers
+    SHORT_WORD = 4
+    LEAST_HIT = 3
+    CONCAT_WORD = concat_word
+    
     supercut_candidates = []
+    if phrase2interval is None:
+        phrase2interval = {}
     segments = []
-    phrase2interval = {}
     num_concat = 0
     for idx, word in tqdm(enumerate(words)):
         if num_concat > 0:
@@ -286,36 +394,53 @@ def same_person_one_sentence(person, sentence):
         phrase = word
         candidates = None
         while idx + num_concat < len(words):
+            if num_concat == CONCAT_WORD:
+                num_concat -= 1
+                break
             if num_concat > 0:
                 phrase += ' ' + words[idx + num_concat]
             # skip short word for long phrase
-            if len(phrase) < 4:
+            if len(phrase) < SHORT_WORD:
                 num_concat += 1
                 continue
             
-            if phrase in phrase2interval:
+            if candidates is None:
+                LEAST_HIT = 0
+            else:
+                LEAST_HIT = 3
+            
+            if phrase in phrase2interval and not phrase2interval[phrase] is None:
+                if phrase2interval[phrase] is None:
+                    num_concat = num_concat - 1 if num_concat != 0 else 0
+                    break
                 candidates = phrase2interval[phrase]
                 segment = phrase
                 num_concat += 1
             else:
                 person_alone_phrase_intrvlcol = get_person_alone_phrase_intrvlcol(person_intrvlcol, phrase)
                 num_intervals = count_intervals(person_alone_phrase_intrvlcol)
-                if num_intervals > 3:
+                if num_intervals > LEAST_HIT:
                     candidates = intrvlcol2list(person_alone_phrase_intrvlcol)
                     phrase2interval[phrase] = candidates
                     segment = phrase
                     num_concat += 1
                 else:
+                    phrase2interval[phrase] = None
                     num_concat = num_concat - 1 if num_concat != 0 else 0
                     break
+        
         # make up for short word            
-        if candidates is None and len(word) < 4:
-            person_alone_phrase_intrvlcol = get_person_alone_phrase_intrvlcol(person_intrvlcol, word)
-            num_intervals = count_intervals(person_alone_phrase_intrvlcol)
-            if num_intervals > 0:
-                candidates = intrvlcol2list(person_alone_phrase_intrvlcol)
-                phrase2interval[word] = candidates
+        if candidates is None and len(word) < SHORT_WORD:
+            if word in phrase2interval:
+                candidates = phrase2interval[word]
                 segment = word
+            else:
+                person_alone_phrase_intrvlcol = get_person_alone_phrase_intrvlcol(person_intrvlcol, word)
+                num_intervals = count_intervals(person_alone_phrase_intrvlcol)
+                if num_intervals > 0:
+                    candidates = intrvlcol2list(person_alone_phrase_intrvlcol)
+                    phrase2interval[word] = candidates
+                    segment = word
         if not candidates is None:
             supercut_candidates.append(candidates)
             segments.append(segment)
@@ -351,4 +476,4 @@ def multi_person_one_phrase(phrase, filters={}):
                 break
         phrase_intrvlcol = VideoIntervalCollection(intrvlcol_withface)
 #         print(len(phrase_intrvlcol))
-    return intrvlcol2list(phrase_intrvlcol, with_duration=True)
+    return intrvlcol2list(phrase_intrvlcol)
