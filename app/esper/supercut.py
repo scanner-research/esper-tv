4a72df510e69caaa66e9108069cac3aee5b4b1dc
Haotian <haotianz@stanford.edu>
2019-01-07 00:18:57 +0000

    add human select pipeline for person sing supercut

--- app/esper/supercut.py
+++ app/esper/supercut.py
@@ -1,4 +1,3 @@
-from django.db.models import F, Q
 # import rekall
 from esper.rekall import *
 from rekall.video_interval_collection import VideoIntervalCollection
@@ -12,11 +11,15 @@ from rekall.list_predicates import length_exactly
 # import caption search
 from esper.captions import *
 
-# import face identities for person search
+# import query sets
 from query.models import Video, Face, FaceIdentity, FaceGender
+from django.db.models import F, Q
 
-# import esper widget for debugging
+# import esper utils and widgets for selection
 from esper.prelude import *
+from esper.widget import *
+from IPython.display import display, clear_output
+import ipywidgets as widgets
 
 import numpy as np
 import random
@@ -29,6 +32,7 @@ from pydub import AudioSegment
 import pysrt
 import re
 import cv2
+import shutil
 
 
 # ============== Help functions ==============    
@@ -37,6 +41,10 @@ def second2time(second, sep=','):
     return '{:02d}:{:02d}:{:02d}{:s}{:03d}'.format(h, m, s, sep, ms)
 
 
+def time2second(time):
+        return time[0]*3600 + time[1]*60 + time[2] + time[3] / 1000.0
+
+    
 def count_intervals(intrvlcol):
     num_intrvl = 0
     for intrvllist in intrvlcol.get_allintervals().values():
@@ -58,7 +66,19 @@ def intrvlcol2list(intrvlcol, with_duration=True):
     return interval_list
 
 
-def select_candidates(intervals, num_sample=1, range=(0.5, 1.5), filter='random'):
+def interval2result(intervals):
+    materialized_result = [
+        {'video': video_id,
+#             'track': t.id,
+         'min_frame': sfid,
+         'max_frame': efid }
+        for video_id, sfid, efid, duration in intervals ]
+    count = len(intervals)
+    groups = [{'type': 'flat', 'label': '', 'elements': [r]} for r in materialized_result]
+    return {'result': groups, 'count': count, 'type': 'Video'}
+
+
+def auto_select_candidates(intervals, num_sample=1, range=(0.5, 1.5), filter='random'):
     durations = [i[-1] for i in intervals]
     median = np.median(durations)
     intervals_regular = [i for i in intervals if i[-1] > range[0] * median and i[-1] < range[1] * median]
@@ -66,13 +86,17 @@ def select_candidates(intervals, num_sample=1, range=(0.5, 1.5), filter='random'
         intervals_regular = [i for i in intervals if i[-1] > 0.5 * median and i[-1] < 1.5 * median]
     # Todo: if regular intervals are not enough
     if filter == 'random':
-        return random.sample(intervals_regular, num_sample)
+        if num_sample == 1:
+            return random.choice(intervals_regular)
+        else:
+            return random.sample(intervals_regular, num_sample)
     elif filter == 'longest':
         durations_regular = [i[-1] for i in intervals_regular]
         return intervals_regular[np.argmax(durations_regular)]
 
     
-def stitch_video_temporal(intervals, out_path, out_duration=None, speed=0.8, dilation=1.0):
+def stitch_video_temporal(intervals, out_path, out_duration=None, dilation=None, speed=None):
+    intervals = intervals.copy()
     def download_video_clip(i):
         video_id, sfid, efid = intervals[i][:3]
         video = Video.objects.filter(id=video_id)[0]
@@ -82,7 +106,8 @@ def stitch_video_temporal(intervals, out_path, out_duration=None, speed=0.8, dil
             video_path = add_fade_out(video_path)
         return video_path
     
-    if dilation < 0.5:
+    in_duration = sum([i[-1] for i in intervals])
+    if dilation < 0.1:
         dilation = None
     if not dilation is None:
         video_id, sfid, efid = intervals[-1][:3]
@@ -91,20 +116,27 @@ def stitch_video_temporal(intervals, out_path, out_duration=None, speed=0.8, dil
     
     # make supercut video 
     clip_paths = par_for(download_video_clip, [i for i in range(len(intervals))])
-    tmp_path = tempfile.NamedTemporaryFile(suffix='.mp4').name
-    concat_videos(clip_paths, tmp_path)
-    if not out_duration is None or speed != 1.0:
-        if not out_duration is None:
-            duration = sum([i[-1] for i in intervals])
-            speed = duration / out_duration
-            print(duration, speed)
+    # concat phrase clips with speed change if need
+    if len(intervals) > 2:
+        tmp_path = tempfile.NamedTemporaryFile(suffix='.mp4').name
+        concat_videos(clip_paths[:-1], tmp_path)
+    else:
+        tmp_path = clip_paths[0]
+    if not out_duration is None:    
+        speed = in_duration / out_duration
+        print(in_duration, out_duration, speed)
         speed = max(0.5, speed)
         speed = min(2.0, speed)
+        tmp_path2 = tempfile.NamedTemporaryFile(suffix='.mp4').name
         cmd = 'ffmpeg -y -i {} -filter_complex "[0:v]setpts={}*PTS[v];[0:a]atempo={}[a]" -map "[v]" -map "[a]" {}' \
-              .format(tmp_path, 1 / speed, speed, out_path)
+              .format(tmp_path, 1 / speed, speed, tmp_path2)
         os.system(cmd)
+        tmp_path = tmp_path2
+    
+    if not dilation is None:
+        concat_videos([tmp_path, clip_paths[-1]], out_path)
     else:
-        os.rename(tmp_path, out_path)
+        shutil.move(tmp_path, out_path)
 
     
 def make_montage_t(args):
@@ -256,7 +288,7 @@ def replace_audio(video_path, audio_path, out_path):
 
 def add_bgm(video_path, bgm_path, out_path, bgm_decrease=2):
     audio_ori = AudioSegment.from_file(video_path, format='mp4')
-    audio_bgm = AudioSegment.from_file(bgm_path, format='wav')
+    audio_bgm = AudioSegment.from_file(bgm_path, format=bgm_path[-3:])
     audio_mix = audio_ori.overlay(audio_bgm - bgm_decrease)
     tmp_path = tempfile.NamedTemporaryFile(suffix='.wav').name
     audio_mix.export(tmp_path, format='wav')
@@ -420,41 +452,120 @@ def get_person_alone_phrase_intervals(person_intrvlcol, phrase):
     
     
 # ============== Applications ============== 
-def single_person_one_song(person_name, lyric_path, out_path, person_intrvlcol=None, cache=True):
-    def time2second(time):
-        return time[0]*3600 + time[1]*60 + time[2] + time[3] / 1000.0
+class SinglePersonSing:
+    def __init__(self, person_name, lyric_path, person_intrvlcol=None):
+        if person_intrvlcol is None:
+            person_intrvlcol = get_person_intrvlcol(person_name, large_face=True, labeler='old')
+        self.person_name = person_name.replace(' ', '_')
+        self.person_intrvlcol = person_intrvlcol
 
-    if person_intrvlcol is None:
-        person_intrvlcol = get_person_intrvlcol(person_name, large_face=True, labeler='old')
-    person_name = person_name.replace(' ', '_')
-    
-    # load lyrics
-    song_name = os.path.basename(lyric_path).replace('.srt', '')
-    lyrics = []
-    subs = pysrt.open(lyric_path)
-    for sub in subs:
-        lyrics.append((sub.text, time2second(tuple(sub.start)[:4]), time2second(tuple(sub.end)[:4])))
+        # load lyrics
+        self.song_name = os.path.basename(lyric_path).replace('.srt', '')
+        lyrics = []
+        subs = pysrt.open(lyric_path)
+        for sub in subs:
+            lyrics.append((sub.text, time2second(tuple(sub.start)[:4]), time2second(tuple(sub.end)[:4])))
+        self.lyrics = lyrics
 
-    cache_path = '/app/result/supercut/cache/{}.pkl'.format(person_name)
-    phrase2interval = pickle.load(open(cache_path, 'rb')) if cache and os.path.exists(cache_path) else {}
+        # load cache
+        cache_path = '/app/result/supercut/cache/{}.pkl'.format(self.person_name)
+        cache = pickle.load(open(cache_path, 'rb')) if os.path.exists(cache_path) else {'candidates':{}, 'selection':{}}
+        self.phrase2candidates, self.phrase2selection = cache['candidates'], cache['selection']
+        self.cache_path = cache_path
     
-    cutting_paths = []
-    for idx, (sentence, start, end) in enumerate(lyrics):
-        supercut_candidates = single_person_one_sentence(person_intrvlcol, sentence, phrase2interval)
-        pickle.dump(phrase2interval, open(cache_path, 'wb'))
+    def search_candidates(self):
+        segments_list = []
+        candidates_list = []
+        for idx, (sentence, start, end) in enumerate(self.lyrics):
+            segments, candidates = single_person_one_sentence(self.person_intrvlcol, sentence, self.phrase2candidates)
+            segments_list.append(segments)
+            candidates_list.append(candidates)
+            self.dump_cache()
+        self.segments_list = segments_list
+        self.candidates_list = candidates_list
+    
+    def select_candidates(self, human_selection=False, duplicate_selection=True, redo_selection=False):
+        self.selections_list = []
+        if not human_selection:
+            for idx, (segments, candidates) in enumerate(zip(self.segments_list, self.candidates_list)):
+                selections = [auto_select_candidates(c, range=(0.99, 1.5), filter='random') for c in candidates]
+                self.selections_list.append(selections)
+        else:
+            self.human_select_candidates(duplicate_selection, redo_selection)
+    
+    def make_supercuts(self, out_path):
+        cutting_paths = []
+        for idx, (sentence, start, end) in enumerate(self.lyrics):
+            selections = self.selections_list[idx]
+            tmp_path = '/app/result/supercut/tmp/{}-{}-{}.mp4'.format(self.person_name, self.song_name, idx)
+            dilation = self.lyrics[idx+1][1] - end if idx != len(self.lyrics) - 1 else 1
+            stitch_video_temporal(selections, tmp_path, out_duration=None, dilation=dilation)
+            cutting_paths.append(tmp_path)
+            print('Concat videos for sentence \"{}\"'.format(sentence))
+            
+    #         if idx == 1:
+    #             break
+        concat_videos(cutting_paths, out_path)
         
-        # sample candidate
-        supercut_intervals = [select_candidates(candidates, range=(0.99, 1.4), filter='longest') for candidates in supercut_candidates]
-        print(supercut_intervals)
+    def dump_cache(self):
+        pickle.dump({'candidates':self.phrase2candidates, 'selection':self.phrase2selection}, open(self.cache_path, 'wb'))
         
-        # concat clips with dilation at the end; global speed change
-        tmp_path = '/app/result/supercut/{}-{}-{}.mp4'.format(person_name, song_name, idx)
-        dilation = lyrics[idx+1][1] - end if idx != len(lyrics) - 1 else 0
-        stitch_video_temporal(supercut_intervals, tmp_path, out_duration=None, dilation=dilation)
-        cutting_paths.append(tmp_path)
-#         if idx == 1:
-#             break
-    concat_videos(cutting_paths, out_path)
+    def human_select_candidates(self, duplicate_selection, redo_selection):
+        if redo_selection:
+            for segments in self.segments_list:
+                for phrase in segments:
+                    if phrase in self.phrase2selection:
+                        del self.phrase2selection[phrase]
+        
+        def launch_widget(idx_sentence, idx_phrase):
+            if idx_phrase >= len(self.segments_list[idx_sentence]):
+                idx_phrase = 0
+                idx_sentence += 1
+                self.selections_list.append([])
+            if idx_sentence >= len(self.segments_list):
+                clear_output()
+                print("Finished all selections")
+                return
+            phrase, candidates = self.segments_list[idx_sentence][idx_phrase], self.candidates_list[idx_sentence][idx_phrase]
+            
+            if phrase in self.phrase2selection and duplicate_selection:
+                self.selections_list[idx_sentence].append(self.phrase2selection[phrase])
+                launch_widget(idx_sentence, idx_phrase + 1)
+                return 
+            else:
+                durations = [i[-1] for i in candidates]
+                candidates_sort = [candidates[idx] for idx in np.argsort(durations)[::-1]]
+                result = interval2result(candidates_sort) 
+            
+            print('Select phrase \"{}\" for sentence \"{}\"'.format(phrase, ' '.join(self.segments_list[idx_sentence])))
+            selection_widget = esper_widget(
+                result, 
+                disable_playback=False, jupyter_keybindings=True,
+                crop_bboxes=True)
+
+            submit_button = widgets.Button(
+                layout=widgets.Layout(width='auto'),
+                description='Save to database',
+                disabled=False,
+                button_style='danger'
+            )
+            def on_submit(b):
+                selection = candidates_sort[selection_widget.selected[0]]
+#                 print(selection_widget.selected, selection)
+                self.selections_list[idx_sentence].append(selection)
+                if not phrase in self.phrase2selection:
+                    self.phrase2selection[phrase] = selection
+                self.dump_cache()
+                clear_output()
+                launch_widget(idx_sentence, idx_phrase + 1)
+            submit_button.on_click(on_submit)
+
+            display(widgets.HBox([submit_button]))
+            display(selection_widget)
+
+        idx_sentence, idx_phrase = 0, 0
+        self.selections_list.append([])
+        launch_widget(idx_sentence, idx_phrase)
     
 
 def single_person_one_sentence(person_intrvlcol, sentence, phrase2interval=None, concat_word=4):
@@ -531,7 +642,7 @@ def single_person_one_sentence(person_intrvlcol, sentence, phrase2interval=None,
             segments.append(segment)
             
     print("Sentence segments: ", segments)
-    return supercut_candidates
+    return segments, supercut_candidates
 
 
 def multi_person_one_phrase(phrase, filters={}):
