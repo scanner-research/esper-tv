616b4fae056e5c0851271496b920ac0df5b638e1
James Hong <james.hong@cs.stanford.edu>
2019-01-10 21:28:35 +0000

    Add visual validation for hair color/length/clothing

--- app/esper/queries/all_faces.py
+++ app/esper/queries/all_faces.py
@@ -6,3 +6,24 @@ def all_faces():
     from query.models import Face
     from esper.widget import qs_to_result
     return qs_to_result(Face.objects.all(), stride=1000)
+
+
+@query("All clothing")
+def all_clothing():
+    from query.models import Clothing
+    from esper.widget import qs_to_result
+    return qs_to_result(Clothing.objects.all(), stride=1000)
+
+
+@query("All haircolor")
+def all_haircolor():
+    from query.models import HairColor
+    from esper.widget import qs_to_result
+    return qs_to_result(HairColor.objects.all(), stride=1000)
+
+
+@query("All hairlength")
+def all_hairlength():
+    from query.models import HairLength
+    from esper.widget import qs_to_result
+    return qs_to_result(HairLength.objects.all(), stride=1000)
