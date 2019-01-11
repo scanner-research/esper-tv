381caf53a3dc78be8ffa95107ffeab0c702cb4a3
James Hong <james.hong@cs.stanford.edu>
2019-01-11 01:08:42 +0000

    Filter out bald people, for white hair

--- app/esper/queries/all_faces.py
+++ app/esper/queries/all_faces.py
@@ -27,3 +27,19 @@ def all_hairlength():
     from query.models import HairLength
     from esper.widget import qs_to_result
     return qs_to_result(HairLength.objects.all(), stride=1000)
+
+
+@query("White hair, not bald")
+def white_hair_not_bald():
+    from query.models import HairColor, HairLength
+    from esper.widget import qs_to_result
+    white_hair_ids = {
+        x['face__id'] for x in 
+        HairColor.objects.filter(color__name='white').values('face__id')[0:1000000:1000]
+    }
+    bald_ids = {
+        x['face__id'] for x in HairLength.objects.filter(
+            length__name='bald', face__id__in=white_hair_ids
+        ).values('face__id')
+    }
+    return qs_to_result(Face.objects.filter(id__in=white_hair_ids - bald_ids))
